ba66b98fa7b030e0c5458d811ac85518cf1fea55
Bryon Baker <bryonbakeraus@gmail.com>
2022-11-23 09:42:49 +1100

    Added helper text to env setup script.

--- tools/setup-onprem-k8s-env.sh
+++ tools/setup-onprem-k8s-env.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 
+echo "Setting up isolated Kubernetes environment"
+echo "NOTE: The command format to run this script is: \". ./setup-onprem-k8s-env.sh\""
+
 export KUBECONFIG=$HOME/.kube/app-modernisation-onprem
 export PS1="\[$(tput setaf 2)\]ONPREM: \[$(tput setaf 7)\]\[$(tput setaf 6)\]\W\\$ \[$(tput setaf 7)\]\[$(tput sgr0)\]"
