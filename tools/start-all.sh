c2ad176502fc157f73fba1f44b2a80f48bcd3b4f
Bryon Baker <bryonbakeraus@gmail.com>
2022-11-23 09:31:38 +1100

    Added ability to detect docker or podman

--- tools/start-all.sh
+++ tools/start-all.sh
@@ -1,7 +1,26 @@
 #! /bin/bash
 
-podman run --rm -d --name pgdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mypassword123 -e POSTGRES_DB=postgresdb -p 5432:5432 postgres
+# Check that one of Podman or Docker is installed.
+echo "Checking for container runtime..."
+runtime=$(which podman)
+inst_status=$?
+if [ $inst_status -eq 0 ]; then
+   echo "Podman is installed"
+   CONTAINER_RUNTIME="podman"
+else
+  runtime=$(which docker)
+  inst_status=$?
+  if [ $inst_status -eq 0 ]; then
+    echo "Docker is installed"
+    CONTAINER_RUNTIME="docker"
+   else
+      echo "ERROR: No container runtime found."
+      exit
+   fi
+fi
 
-podman run --rm -d -e POSTGRES_SERVICE_HOST=$(hostname) -e POSTGRES_DB=postgresdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mypassword123 -p 8080:8080 --name backend quay.io/bfarr/jboss-demo-backend
+$CONTAINER_RUNTIME run --rm -d --name pgdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mypassword123 -e POSTGRES_DB=postgresdb -p 5432:5432 postgres
 
-podman run --rm -d -e BACKEND_PROVIDER_URL=remote+http://$(hostname):8080 -p 8180:8080 --name frontend quay.io/bfarr/jboss-demo-frontend
+$CONTAINER_RUNTIME run --rm -d -e POSTGRES_SERVICE_HOST=$(hostname) -e POSTGRES_DB=postgresdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mypassword123 -p 8080:8080 --name backend quay.io/bfarr/jboss-demo-backend
+
+$CONTAINER_RUNTIME run --rm -d -e BACKEND_PROVIDER_URL=remote+http://$(hostname):8080 -p 8180:8080 --name frontend quay.io/bfarr/jboss-demo-frontend
