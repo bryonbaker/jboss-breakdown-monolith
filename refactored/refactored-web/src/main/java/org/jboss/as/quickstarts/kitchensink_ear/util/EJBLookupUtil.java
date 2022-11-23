a3b24917c36cf4429b9ec4126046c2c87a7e2777
bfarr-rh <bfarr@redhat.com>
2022-11-23 11:07:59 +1100

    Refactored to include staged refactoring projects

---
+++ refactored/refactored-web/src/main/java/org/jboss/as/quickstarts/kitchensink_ear/util/EJBLookupUtil.java
@@ -0,0 +1,6 @@
+package org.jboss.as.quickstarts.kitchensink_ear;
+
+public class Params {
+
+    public static String EJB_LOOKUP = "ejb:backend/ejb/MemberRepository!org.jboss.as.quickstarts.kitchensink_ear.data.MemberRepositoryIF";
+}
