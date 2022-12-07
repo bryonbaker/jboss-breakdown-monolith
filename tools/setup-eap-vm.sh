cdb8be4d8c697194ac3848edceacbc4f38274558
bfarr-rh <bfarr@redhat.com>
2022-12-07 18:05:57 +1100

    refresh page on view queue hide

--- tools/setup-eap-vm.sh
+++ tools/setup-eap-vm.sh
@@ -6,6 +6,10 @@ cp ../original-monolith/original-ear/target/obank.ear $JBOSS_HOME/standalone/dep
 cp ../modular/modular-ear/target/modular-ear.ear $JBOSS_HOME/standalone/deployments
 cp ../refactored/refactored-ear-frontend/target/frontend.ear $JBOSS_HOME/standalone/deployments
 cp ../refactored/refactored-ear-backend/target/backend.ear $JBOSS_HOME/standalone/deployments
+cp ../files/standalone.xml $JBOSS_HOME/standalone/configuration/standalone.xml
+mkdir -p $JBOSS_HOME/modules/org/postgresql/main
+cp ../files/pgdatasource/module.xml $JBOSS_HOME/modules/org/postgresql/main/module.xml
+cp ../files/pgdatasource/postgresql-42.5.0.jar $JBOSS_HOME/modules/org/postgresql/main/postgresql-42.5.0.jar
 
 podman run --rm=true --name pgdb -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=mypassword123 -e POSTGRES_DB=postgresdb -d -p 5432:5432 docker.io/library/postgres:latest
 
@@ -15,4 +19,4 @@ export POSTGRES_DB=postgresdb
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=mypassword123
 
-../../jboss-eap-7.4/bin/standalone.sh
+$JBOSS_HOME/bin/standalone.sh -b 0.0.0.0
