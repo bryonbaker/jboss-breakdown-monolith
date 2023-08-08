08ca87e37626230062bd6dac40dd4eae1c89ac0f
bfarr-rh <bfarr@redhat.com>
2023-08-08 17:03:00 +1000

    added quick script for db on prem

---
+++ tools/quick-db-demo-scripts/remote-db.sh
@@ -0,0 +1,27 @@
+#oc login
+#curl https://skupper.io/install.sh | sh
+
+oc new-project app-modernisation
+
+skupper init --enable-console --enable-flow-collector --site-name onpremises --console-auth=internal --console-user=admin --console-password=password
+
+oc apply -f https://raw.githubusercontent.com/bfarr-rh/jboss-breakdown-monolith/master/yaml/all.yaml
+
+podman run --rm=true --name pgdb -e POSTGRES_USER=demo -e POSTGRES_PASSWORD=mypassword123 -e POSTGRES_DB=demo-db -d -p 5432:5432 docker.io/library/postgres:10
+
+skupper gateway expose db 127.0.0.1 5432 --type podman
+
+# Go to ui
+
+echo "Select * from Registrant" | podman exec -i pgdb psql -U demo -d demo-db
+
+#clean up
+skupper gateway delete
+
+rm -rf ~/.local/share/skupper
+
+podman kill --all 
+
+podman system prune --force
+
+oc delete project app-modernisation
