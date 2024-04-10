ab69891d8493f5aa36da0d28b282ef3ff1c07fd3
bryonbaker <bryonbakeraus@gmail.com>
2024-04-10 15:09:13 +1000

    Added podman network

--- tools/start-all.sh
+++ tools/start-all.sh
@@ -33,13 +33,16 @@ FRONTEND_IMAGE=quay.io/bfarr/jboss-demo-frontend
 HOST=host.containers.internal
 echo $HOST
 
+podman network create banking-app
+
 # If you want the data to persist across sessions use this option:
 # -v /host/db/path:/var/lib/pgsql/data
-$CONTAINER_RUNTIME run --rm -d --name pgdb -e POSTGRESQL_USER=$PGSQL_USER -e POSTGRESQL_PASSWORD=$PGSQL_PASSWORD -e POSTGRESQL_DATABASE=$PGSQL_DB -p 5432:5432 $POSTRES_IMAGE
+#$CONTAINER_RUNTIME run --rm -d --name pgdb -e POSTGRESQL_USER=$PGSQL_USER -e POSTGRESQL_PASSWORD=$PGSQL_PASSWORD -e POSTGRESQL_DATABASE=$PGSQL_DB -p 5432:5432 $POSTRES_IMAGE
+$CONTAINER_RUNTIME run --rm -d --name pgdb --hostname pgdb --network banking-app -e POSTGRESQL_USER=$PGSQL_USER -e POSTGRESQL_PASSWORD=$PGSQL_PASSWORD -e POSTGRESQL_DATABASE=$PGSQL_DB -p 5432:5432 $POSTRES_IMAGE
 sleep 5
 
 #$CONTAINER_RUNTIME run --rm -d -e POSTGRES_SERVICE_HOST=$(hostname) -e POSTGRES_DB=$PGSQL_DB -e POSTGRES_USER=$PGSQL_USER -e POSTGRES_PASSWORD=$PGSQL_PASSWORD -p 8080:8080 --name backend $BACKEND_IMAGE
-$CONTAINER_RUNTIME run --rm -d -e POSTGRES_SERVICE_HOST=$HOST -e POSTGRES_DB=$PGSQL_DB -e POSTGRES_USER=$PGSQL_USER -e POSTGRES_PASSWORD=$PGSQL_PASSWORD -p 8080:8080 --name backend $BACKEND_IMAGE
+$CONTAINER_RUNTIME run --rm -d -e POSTGRES_SERVICE_HOST=pgdb -e POSTGRES_DB=$PGSQL_DB -e POSTGRES_USER=$PGSQL_USER -e POSTGRES_PASSWORD=$PGSQL_PASSWORD -p 8080:8080 --name backend --hostname backend  --network banking-app  $BACKEND_IMAGE
 
 #$CONTAINER_RUNTIME run --rm -d -e BACKEND_PROVIDER_URL=remote+http://$(hostname):8080 -p 8180:8080 --name frontend $FRONTEND_IMAGE
-$CONTAINER_RUNTIME run --rm -d -e BACKEND_PROVIDER_URL=remote+http://$HOST:8080 -p 8180:8080 --name frontend $FRONTEND_IMAGE
+$CONTAINER_RUNTIME run --rm -d -e BACKEND_PROVIDER_URL=remote+http://backend:8080 -p 8180:8080 --hostname frontend --name frontend  --network banking-app $FRONTEND_IMAGE
